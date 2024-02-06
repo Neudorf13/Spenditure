f0b02584a8403bab0116d04e126a30a487523947
Trevor Neudorf <Neudorft@myumanitoba.ca>
2024-02-05 21:52:36 -0600

    Implemented Sorting functions to ReportManager + associated tests

--- app/src/test/java/com/spenditure/business/ReportManagerTest.java
+++ app/src/test/java/com/spenditure/business/ReportManagerTest.java
@@ -22,9 +22,7 @@ public class ReportManagerTest {
 
     @Before
     public void setup() {
-        //CategoryStub.cleanup();
         this.reportManager = new ReportManager(true);
-
     }
 
     @Test
@@ -33,7 +31,6 @@ public class ReportManagerTest {
         double category1 = reportManager.getPercentForCategory(1);
         double category2 = reportManager.getPercentForCategory(2);
         double category3 = reportManager.getPercentForCategory(3);
-        double category4 = reportManager.getPercentForCategory(4);
         double sum = category1 + category2 + category3;
 
         assertEquals("Expected category 1 to take up approximately 19.72% of total transaction cost", category1,19.72,0.1);
@@ -57,13 +54,11 @@ public class ReportManagerTest {
         assertEquals("Expected transaction sum for Category 1 to be 440.75",category1Total,440.75,0);
         assertEquals("Expected transaction sum for Category 2 to be 416.74",category2Total,416.74,0);
         assertEquals("Expected transaction sum for Category 3 to be 1376.45",category3Total,1376.45,0);
-
     }
 
 
     @Test
     public void testAverageForCategory() {
-        //should have average for
         double testCategory1 = reportManager.getAverageForCategory(1);
         double testCategory2 = reportManager.getAverageForCategory(2);
         double testCategory3 = reportManager.getAverageForCategory(3);
@@ -71,11 +66,45 @@ public class ReportManagerTest {
         assertEquals("Expected transaction average for Category 1 to be approximately 110.19",testCategory1,110.19,0.1);
         assertEquals("Expected transaction average for Category 2 to be approximately 104.19",testCategory2,104.19,0.1);
         assertEquals("Expected transaction average for Category 3 to be approximately 229.41",testCategory3,229.41,0.1);
-
     }
 
     @Test
-    public void testThis() {
-        //ArrayList<Transaction> test = testStub.getTransactionByCategoryID(1);
+    public void testSortByTotal() {
+        ArrayList<Category> descendingCategoryList = reportManager.sortByTotal(true);
+        assertEquals("Expected Category: 'Hang out'",descendingCategoryList.get(0).getName(), "Hang out");
+        assertEquals("Expected Category: 'Grocery'",descendingCategoryList.get(1).getName(), "Grocery");
+        assertEquals("Expected Category: 'Food'",descendingCategoryList.get(2).getName(), "Food");
+
+        ArrayList<Category> ascendingCategoryList = reportManager.sortByTotal(false);
+        assertEquals("Expected Category: 'Food'",ascendingCategoryList.get(0).getName(), "Food");
+        assertEquals("Expected Category: 'Grocery'",ascendingCategoryList.get(1).getName(), "Grocery");
+        assertEquals("Expected Category: 'Hang out'",ascendingCategoryList.get(2).getName(), "Hang out");
     }
+
+    @Test
+    public void testSortByPercent() {
+        ArrayList<Category> descendingCategoryList = reportManager.sortByPercent(true);
+        assertEquals("Expected Category: 'Hang out'",descendingCategoryList.get(0).getName(), "Hang out");
+        assertEquals("Expected Category: 'Grocery'",descendingCategoryList.get(1).getName(), "Grocery");
+        assertEquals("Expected Category: 'Food'",descendingCategoryList.get(2).getName(), "Food");
+
+        ArrayList<Category> ascendingCategoryList = reportManager.sortByPercent(false);
+        assertEquals("Expected Category: 'Food'",ascendingCategoryList.get(0).getName(), "Food");
+        assertEquals("Expected Category: 'Grocery'",ascendingCategoryList.get(1).getName(), "Grocery");
+        assertEquals("Expected Category: 'Hang out'",ascendingCategoryList.get(2).getName(), "Hang out");
+    }
+
+    @Test
+    public void testSortByAverage() {
+        ArrayList<Category> descendingCategoryList = reportManager.sortByAverage(true);
+        assertEquals("Expected Category: 'Hang out'",descendingCategoryList.get(0).getName(), "Hang out");
+        assertEquals("Expected Category: 'Grocery'",descendingCategoryList.get(1).getName(), "Grocery");
+        assertEquals("Expected Category: 'Food'",descendingCategoryList.get(2).getName(), "Food");
+
+        ArrayList<Category> ascendingCategoryList = reportManager.sortByAverage(false);
+        assertEquals("Expected Category: 'Food'",ascendingCategoryList.get(0).getName(), "Food");
+        assertEquals("Expected Category: 'Grocery'",ascendingCategoryList.get(1).getName(), "Grocery");
+        assertEquals("Expected Category: 'Hang out'",ascendingCategoryList.get(2).getName(), "Hang out");
+    }
+
 }
