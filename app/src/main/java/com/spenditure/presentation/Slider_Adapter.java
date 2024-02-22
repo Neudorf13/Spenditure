94c3545b50b99f1dbf4cbef21c82ea8c474b1fc5
Bảo Ngô <ngot1@myumanitoba.ca>
2024-02-22 00:42:31 -0600

    Finish UI for feature 4
    Fill in data to UI
    Note: code will be clean in the next commit.

--- app/src/main/java/com/spenditure/presentation/Slider_Adapter.java
+++ app/src/main/java/com/spenditure/presentation/Slider_Adapter.java
@@ -12,28 +12,32 @@ import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
 import com.example.spenditure.R;
+import com.spenditure.logic.ReportManager;
+import com.spenditure.object.MainCategory;
+
+import java.util.List;
+
 
 public class Slider_Adapter extends PagerAdapter {
     Context context;
     LayoutInflater inflater;
-
-    public Slider_Adapter(Context context){
-        this.context = context;
-    }
-    private String[] list_tittle = {
-            "Food",
-            "Grocery",
-            "Hang out"
-    };
-
+    private ReportManager reportManager;
+    private List<MainCategory> categoryList;
     private int[] list_bg_color = {
-           R.drawable.gradient_background_dark_blue,
-            R.drawable.gradient_background_light_green
+      R.drawable.gradient_background_light_green,
+      R.drawable.gradient_background_dark_blue
     };
+
+    public Slider_Adapter(Context context, List<MainCategory> categoryList){
+        this.context = context;
+        this.reportManager = new ReportManager(true);
+        this.categoryList = categoryList;
+    }
+
 
     @Override
     public int getCount() {
-        return list_tittle.length;
+        return reportManager.countAllCategories();
     }
 
     @Override
@@ -47,8 +51,27 @@ public class Slider_Adapter extends PagerAdapter {
         inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.report_slide,container,false);
         LinearLayout linearLayout = view.findViewById(R.id.report_slider_layout);
+        MainCategory currCategory = categoryList.get(position);
+
+        String countTransactionsString = reportManager.countTransactionsByCategory(currCategory.getID())+ " transactions";
+        String totalTransactionsString= "$"+ reportManager.getTotalForCategory(currCategory.getID());
+        String averageString= "$"+ reportManager.getAverageForCategory(currCategory.getID());
+        String percentageString = reportManager.getPercentForCategory(currCategory.getID()) + "%";
+
+
         TextView tittle = view.findViewById(R.id.slide_tittle);
-        tittle.setText(list_tittle[position]);
+        TextView countTransactions = view.findViewById(R.id.textview_catReport_transactionsCount);
+        TextView totalTransactions = view.findViewById(R.id.textview_catReport_total);
+        TextView average = view.findViewById(R.id.textview_catReport_average);
+        TextView percentage = view.findViewById(R.id.textview_catReport_percentage);
+
+        tittle.setText(currCategory.getName());
+        countTransactions.setText(countTransactionsString);
+        totalTransactions.setText(totalTransactionsString);
+        average.setText(averageString);
+        percentage.setText(percentageString);
+
+
         linearLayout.setBackgroundResource(list_bg_color[position % list_bg_color.length]);
         container.addView(view);
         return view;
--- app/src/main/java/com/spenditure/presentation/ViewReportActivity.java
+++ app/src/main/java/com/spenditure/presentation/ViewReportActivity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,11 +19,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.spenditure.databinding.ActivityMainBinding;
+import com.spenditure.logic.CategoryHandler;
+import com.spenditure.logic.ReportManager;
+import com.spenditure.object.MainCategory;
 import com.spenditure.presentation.category.ViewCategoryActivity;
 import com.spenditure.presentation.transaction.CreateTransactionActivity;
 import com.spenditure.presentation.transaction.ViewTransactionsActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ViewReportActivity extends AppCompatActivity {
@@ -30,50 +35,21 @@ public class ViewReportActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private ViewPager viewPager;
     private Slider_Adapter adapter;
+    private ReportManager reportManager;
+    private final String[] custom_option = {"Report by average","Report by total","Report by percentage"};
+    private CategoryHandler categoryHandler;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        reportManager = new ReportManager(true);
+        categoryHandler = new CategoryHandler(true);
 
-        viewPager = findViewById(R.id.viewpager_report);
-        adapter = new Slider_Adapter(this);
-        viewPager.setAdapter(adapter);
-
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        navView.setSelectedItemId(R.id.navigation_home);
-        Spinner spinner = findViewById(R.id.spinner_report);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(ViewReportActivity.this,item,Toast.LENGTH_SHORT).show();
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        ArrayList<String> choices = new ArrayList<>();
-        choices.add("Report by average");
-        choices.add("Report by total");
-        choices.add("Report by percentage");
-        ArrayAdapter<String>adapter = new ArrayAdapter<>(this, R.layout.custom_snipper_report,choices);
-
-        adapter.setDropDownViewResource( android.R.layout.select_dialog_singlechoice);
-        spinner.setAdapter(adapter);
-
-
-
-
+        handleGeneralReport();
+        handleCustomCategoryReport();
+        handleCategoriesReport();
         navBarHandling();
     }
 
@@ -98,5 +74,78 @@ public class ViewReportActivity extends AppCompatActivity {
                 return false;
             }
         }));
+
     }
+
+    private void handleGeneralReport(){
+        String spendString = "You've spent $ " + reportManager.getTotalForAllTransactions();
+        String makeTransactions = "You've made " + reportManager.countAllTransactions() + " transactions";
+        TextView short_text = findViewById(R.id.textview_summary_report_short);
+        TextView long_text = findViewById(R.id.textview_summary_report_long);
+
+        if (spendString.length() > makeTransactions.length()){
+            long_text.setText(spendString);
+            short_text.setText(makeTransactions);
+        }else{
+            long_text.setText(makeTransactions);
+            short_text.setText(spendString);
+        }
+    }
+
+    private void handleCustomCategoryReport(){
+        Spinner spinner = findViewById(R.id.spinner_report);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(ViewReportActivity.this,item,Toast.LENGTH_SHORT).show();
+
+                String spendMostString = "Mostly spending on ";
+                String spendLeastString = "Least spending on ";
+                List<MainCategory>categories;
+
+                switch (position){
+                    case 2:
+                        categories = reportManager.sortByPercent(true);
+                        break;
+                    case 1:
+                        categories = reportManager.sortByTotal(true);
+                        break;
+                    default:
+                        categories = reportManager.sortByAverage(true);
+                }
+
+                if(categories.size() == 0){
+                    findViewById(R.id.imageView_check_custom).setVisibility(View.INVISIBLE);
+                    ((TextView)findViewById(R.id.textview_custom_report_long)).setText("Group transactions to categories to see the report.");
+                }else{
+                    spendMostString += categories.get(0).getName();
+                    spendLeastString += categories.get(categories.size()-1).getName();
+                    if( spendMostString.length() > spendLeastString.length()){
+                        ((TextView)findViewById(R.id.textview_custom_report_long)).setText(spendMostString);
+                        ((TextView)findViewById(R.id.textview_custom_report_short)).setText(spendLeastString);
+                    }else{
+                        ((TextView)findViewById(R.id.textview_custom_report_long)).setText(spendLeastString);
+                        ((TextView)findViewById(R.id.textview_custom_report_short)).setText(spendMostString);
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<String>adapter = new ArrayAdapter<>(this, R.layout.custom_snipper_report,custom_option);
+        adapter.setDropDownViewResource( android.R.layout.select_dialog_singlechoice);
+        spinner.setAdapter(adapter);
+    }
+
+    private void handleCategoriesReport(){
+        viewPager = findViewById(R.id.viewpager_report);
+        adapter = new Slider_Adapter(this,categoryHandler.getAllCategory());
+        viewPager.setAdapter(adapter);
+    }
+
 }
