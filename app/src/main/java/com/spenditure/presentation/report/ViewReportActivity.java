e4679b3ffb99cb8de907b2b3bfc516f1d24f211c
Bảo Ngô <ngot1@myumanitoba.ca>
2024-02-24 16:49:21 -0600

    Update UI for feature 4
    Add all UI components for feature 4
    Note: static UI.

--- app/src/main/java/com/spenditure/presentation/report/ViewReportActivity.java
+++ app/src/main/java/com/spenditure/presentation/report/ViewReportActivity.java
@@ -1,15 +1,21 @@
 package com.spenditure.presentation.report;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.GridLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.spenditure.R;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,9 +26,6 @@ import com.spenditure.logic.CategoryHandler;
 import com.spenditure.logic.ReportManager;
 import com.spenditure.object.MainCategory;
 import com.spenditure.presentation.BottomNavigationHandler;
-import com.spenditure.presentation.category.ViewCategoryActivity;
-import com.spenditure.presentation.transaction.CreateTransactionActivity;
-import com.spenditure.presentation.transaction.ViewTransactionsActivity;
 
 import java.util.List;
 
@@ -30,11 +33,13 @@ import java.util.List;
 public class ViewReportActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private ViewPager viewPager;
+    private ViewPager viewPagerCategory;
     private SliderAdapter adapter;
     private ReportManager reportManager;
     private final String[] custom_option = {"Report by average","Report by total","Report by percentage"};
+    private final String[] time_base_option = {"Report by year breaking into month","Report by month breaking into weeks"};
     private CategoryHandler categoryHandler;
+    private ViewPager viewPagerCustom;
 
 
     @Override
@@ -47,7 +52,64 @@ public class ViewReportActivity extends AppCompatActivity {
         handleGeneralReport();
         handleCustomCategoryReport();
         handleCategoriesReport();
+        handleTimebaseReport();
+        handleCustomDateReport();
         navBarHandling();
+    }
+
+    private void handleCustomDateReport(){
+        Button fromButton = findViewById(R.id.button_report_from);
+        Button toButton = findViewById(R.id.button_report_to);
+        fromButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog(fromButton);
+            }
+        });
+
+        toButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog(toButton);
+            }
+        });
+    }
+
+    private void handleTimebaseReport(){
+        ShimmerFrameLayout shimmerFrameLayout = findViewById(R.id.shimmer_timebase_report);
+        viewPagerCustom = findViewById(R.id.gridlayout_timebase_report);
+        Spinner spinner = findViewById(R.id.spinner_timebase_report);
+
+
+        SliderAdapter adapterCustom= new SliderAdapter(this,categoryHandler.getAllCategory());
+        viewPagerCustom.setAdapter(adapterCustom);
+
+
+        ArrayAdapter<String>adapter = new ArrayAdapter<>(this, R.layout.custom_snipper_report,time_base_option);
+        adapter.setDropDownViewResource( android.R.layout.select_dialog_singlechoice);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                viewPagerCustom.setVisibility(View.INVISIBLE);
+                shimmerFrameLayout.setVisibility(View.VISIBLE);
+                shimmerFrameLayout.startShimmerAnimation();
+
+                Handler handler = new Handler();
+                handler.postDelayed(()->{
+                    viewPagerCustom.setVisibility(View.VISIBLE);
+                    shimmerFrameLayout.stopShimmerAnimation();
+                    shimmerFrameLayout.setVisibility(View.INVISIBLE);
+                },3000);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     // Handle the bottom navigation bar
@@ -108,6 +170,7 @@ public class ViewReportActivity extends AppCompatActivity {
 
     private void handleCustomCategoryReport(){
         Spinner spinner = findViewById(R.id.spinner_report);
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -157,9 +220,20 @@ public class ViewReportActivity extends AppCompatActivity {
     }
 
     private void handleCategoriesReport(){
-        viewPager = findViewById(R.id.viewpager_report);
+        viewPagerCategory = findViewById(R.id.viewpager_report);
         adapter = new SliderAdapter(this,categoryHandler.getAllCategory());
-        viewPager.setAdapter(adapter);
+        viewPagerCategory.setAdapter(adapter);
+    }
+
+    private void openDialog(Button button){
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String displayDay = String.valueOf(dayOfMonth ) + "/"  + String.valueOf(month+ 1) + "/" + String.valueOf(year);
+                button.setText(displayDay);
+            }
+        }, 2024, 0, 15);
+        dialog.show();
     }
 
 }
