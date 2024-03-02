be3d3b6ab4cd9f2e276c48a910ffed0a4c6d8ac3
jfriesen29 <jillianfriesen19@gmail.com>
2024-03-02 16:09:58 -0600

    Login activity functional, set to open on launch

--- app/src/main/java/com/spenditure/presentation/LoginActivity.java
+++ app/src/main/java/com/spenditure/presentation/LoginActivity.java
@@ -2,22 +2,33 @@ package com.spenditure.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.spenditure.R;
+import com.spenditure.logic.CategoryHandler;
+import com.spenditure.logic.UserManager;
+import com.spenditure.logic.exceptions.InvalidUserInformationException;
+import com.spenditure.presentation.report.ViewReportActivity;
+import com.spenditure.presentation.transaction.EditTransactionActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private String username;
     private String password;
+    private UserManager userManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userManager = new UserManager(true);
         setContentView(R.layout.activity_login);
+
+        setUpLoginButton();
     }
 
     private void setUpLoginButton() {
@@ -30,7 +41,19 @@ public class LoginActivity extends AppCompatActivity {
                 EditText pass = (EditText) findViewById(R.id.edittext_password);
                 password = pass.getText().toString();
 
-                // TODO: call validation method in the logic layer
+                try {
+                    int userID = userManager.login(username, password);
+
+                    if (userID != 0){
+                        // Login
+                        Intent newIntent = new Intent(getApplicationContext(), ViewReportActivity.class);
+                        startActivity(newIntent);
+                    }
+                } catch (InvalidUserInformationException e){
+                    Toast.makeText(LoginActivity.this, e.getMessage(),Toast.LENGTH_LONG).show();
+                    user.setText("");
+                    pass.setText("");
+                }
             }
         });
     }
