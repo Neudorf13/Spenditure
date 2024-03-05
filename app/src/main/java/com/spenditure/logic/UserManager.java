b7d08de3e53344953b28abd0598d6d7477bb7227
toranp <pillayt@myumanitoba.ca>
2024-03-05 17:47:37 -0600

    Implemented further validation in UserManager, reworked functions, and added more validation to UserValidator. Deleted StringInputValidator

--- app/src/main/java/com/spenditure/logic/UserManager.java
+++ app/src/main/java/com/spenditure/logic/UserManager.java
@@ -1,5 +1,7 @@
 package com.spenditure.logic;
 
+import static com.spenditure.logic.UserValidator.*;
+
 import com.spenditure.application.Services;
 import com.spenditure.database.UserPersistence;
 import com.spenditure.logic.exceptions.InvalidStringFormat;
@@ -18,69 +20,146 @@ public class UserManager implements IUserManager{
         accountPersistence = Services.getUserPersistence(getStubDB);
 
     }
+
+    /*
+
+        login
+
+        Given a username and password as Strings, checks to see if they are valid, and then
+        sets the corresponding User ID as the current user.
+
+     */
     public int login(String username, String password) throws InvalidUserInformationException {
-        if(username == null || password == null || username.equals("") || password.equals("")){
-            throw new InvalidUserInformationException("Please provide username and password");
-        }else{
-            if(userID <0){
-                int userIDReturn = accountPersistence.login(username,password);
-                UserManager.userID = userIDReturn;
-                return userIDReturn;
-            }else{
-                throw new InvalidUserInformationException("Please logout before login");
-            }
+
+        validateUsernameAndPassword(username, password);
+
+        if(userID < 0) {
+            int userIDReturn = accountPersistence.login(username,password);
+            UserManager.userID = userIDReturn;
+
+            return userIDReturn;
+
+        } else {
+            //Someone is already logged in
+            throw new InvalidUserInformationException("Please log out before logging in.");
 
         }
-    }; //Return user id
-    public String getUserName(int userID) throws InvalidUserInformationException{
-        if (userID < 0) throw new InvalidUserInformationException("Please login");
+
+    } //Return user id
+
+    /*
+
+        getUserName
+
+        Provides a username for a given user ID.
+
+     */
+    public String getUserName(int userID) throws InvalidUserInformationException {
+
+        if (userID < 0)
+            throw new InvalidUserInformationException("No users are currently logged in; please log in first.");
+
         return accountPersistence.getUserName(userID);
-    };
-    public boolean changePassword(int userID, String oldPassword, String newPassword) throws InvalidUserInformationException{
-        if(oldPassword == null || newPassword == null || oldPassword == "" || newPassword == ""){
-            throw new InvalidUserInformationException("Please provide current and new password");
-        }else{
-            if(userID < 0) throw new InvalidUserInformationException("Please login");
-            return accountPersistence.changePassword(userID,oldPassword,newPassword);
-        }
-    };
-    public boolean changeUsername(int providedUserID, String newUsername)throws InvalidUserInformationException, InvalidStringFormat {
-        if(newUsername == null || newUsername == "" ){
-            throw new InvalidUserInformationException("Please provide new username");
-        }else{
-            if(providedUserID < 0) throw new InvalidUserInformationException("Please login");
-            StringInputValidator.validateInputString(newUsername);
-            return accountPersistence.changeUsername(providedUserID,newUsername);
-        }
-    };
-    public int register(String username, String password)throws InvalidStringFormat{
-        if(username == null || password == null || username == "" || password == ""){
-            throw new InvalidUserInformationException("Please provide username and password");
-        }else{
-            StringInputValidator.validateInputString(username);
-            int newUserID = accountPersistence.register(username,password,null);
-            UserManager.userID = newUserID;
-            return newUserID;
-        }
-    };
 
-    public void logout() throws InvalidUserInformationException{
-        if (userID >= 0){
+    }
+
+    /*
+
+        changePassword
+
+        For a logged in user, enables user to change the account's password to something else.
+        Old password is required. Both are taken as Strings
+
+     */
+    public boolean changePassword(int userID, String oldPassword, String newPassword) throws InvalidUserInformationException {
+
+        validatePassword(oldPassword);
+        validatePassword(newPassword);
+
+        if(userID < 0)
+            throw new InvalidUserInformationException("No users are logged in; please log in before changing your password.");
+
+        return accountPersistence.changePassword(userID,oldPassword,newPassword);
+
+    }
+
+    /*
+
+        changeUsername
+
+        For a logged in user, enables user to change the account's username to something else.
+        The new username is taken as a string.
+
+     */
+    public boolean changeUsername(int providedUserID, String newUsername)
+            throws InvalidUserInformationException, InvalidStringFormat {
+
+        validateUsername(newUsername);
+
+        if(providedUserID < 0)
+            throw new InvalidUserInformationException("No users are logged in; please log in before changing your username.");
+
+        return accountPersistence.changeUsername(providedUserID,newUsername);
+
+    }
+
+    /*
+
+        register
+
+        Creates a new user account with the given Strings for username and password. Returns the
+        new user's User ID.
+
+     */
+    public int register(String username, String password) throws InvalidStringFormat {
+
+        validateUsernameAndPassword(username, password);
+
+        int newUserID = accountPersistence.register(username,password,null);
+
+        UserManager.userID = newUserID;
+
+        return newUserID;
+
+    }
+
+    /*
+
+        logout
+
+        Sets active user ID to -1, signifying that no user is currently logged in.
+
+     */
+    public void logout() throws InvalidUserInformationException {
+
+        if (userID >= 0) {
             userID = -1;
-        }else{
-            throw  new InvalidUserInformationException("Please login before logout");
+
+        } else {
+            throw new InvalidUserInformationException("Cannot log out, as no users are currently logged in. Please log in first.");
+
         }
     }
 
-    public static int getUserID() throws InvalidUserInformationException{
-        if(userID > 0) {
+    /*
+
+        getUserID
+
+        Returns the logged-in user's User ID.
+
+     */
+    public static int getUserID() throws InvalidUserInformationException {
+
+        if(userID >= 0) {
             return userID;
-        }else {
-            throw new InvalidUserInformationException("Please login before get user id " + userID);
+
+        } else {
+            throw new InvalidUserInformationException("No users are logged in. Please log in before requesting a User ID.");
+
         }
     }
 
-    //For testing purpose
+    //For testing purposes
     public static void cleanup(){
         Services.restartAccountDB(true);
         UserManager.userID = -1;
