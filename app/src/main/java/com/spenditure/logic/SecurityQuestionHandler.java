48b68dc69dd394704d0d7ef54e19733ad2c183a6
toranp <pillayt@myumanitoba.ca>
2024-03-19 14:43:09 -0500

    Implemented SecurityQuestionHandler, to enable UI layer to communicate with DB layer with regard to Security Questions

---
+++ app/src/main/java/com/spenditure/logic/SecurityQuestionHandler.java
@@ -0,0 +1,55 @@
+package com.spenditure.logic;
+
+import com.spenditure.application.Services;
+import com.spenditure.database.SecurityQuestionPersistence;
+
+import java.util.ArrayList;
+
+public class SecurityQuestionHandler {
+
+    private SecurityQuestionPersistence dataAccessSecurityQuestion;
+
+    public SecurityQuestionHandler(boolean getStubDB) {
+
+        if(dataAccessSecurityQuestion == null)
+            dataAccessSecurityQuestion = Services.getSecurityQuestionPersistence(getStubDB);
+
+    }
+
+    public void cleanup(boolean getStubDB) {
+
+        Services.restartSecurityQuestionDB(getStubDB);
+
+        dataAccessSecurityQuestion = Services.getSecurityQuestionPersistence(getStubDB);
+
+    }
+
+    /*
+
+        getSecurityQuestion
+
+        Given a Security Question ID, retrieves that Security Question (if any).
+
+     */
+    public String getSecurityQuestion(int securityQuestionID) {
+
+        assert( securityQuestionID > 0 );
+
+        return dataAccessSecurityQuestion.getSecurityQuestion(securityQuestionID);
+
+    }
+
+    /*
+
+        getAllSecurityQuestions
+
+        Returns an ArrayList with all the Security Questions.
+
+     */
+    public ArrayList<String> getAllSecurityQuestions() {
+
+        return dataAccessSecurityQuestion.getAllSecurityQuestions();
+
+    }
+
+}
