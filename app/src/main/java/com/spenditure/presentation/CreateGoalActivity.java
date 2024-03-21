c28203ad5dbfd3fd24c3182225cda30d12fd682e
jfriesen29 <jillianfriesen19@gmail.com>
2024-03-21 13:55:34 -0500

    Addin required activity files for the Goal Tracker

---
+++ app/src/main/java/com/spenditure/presentation/CreateGoalActivity.java
@@ -0,0 +1,16 @@
+package com.spenditure.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.spenditure.R;
+
+public class CreateGoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_goal);
+    }
+}
---
+++ app/src/main/java/com/spenditure/presentation/GoalListViewActivity.java
@@ -0,0 +1,16 @@
+package com.spenditure.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.spenditure.R;
+
+public class GoalListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_list_view);
+    }
+}
